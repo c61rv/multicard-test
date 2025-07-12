c3f418a892fbb0266bba00a5d614d79472eb4ce0
Mansur <m.sabirov@alex-its.uz>
2025-07-12 17:16:23 +0500

    Создание основного визуала

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,30 @@
-import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-export default createVuetify()
+export default createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+  },
+  theme: {
+    themes: {
+      light: {
+        dark: false,
+        colors: {
+          primary: '#424242',
+          secondary: '#c1121f',
+          accent: '#82B1FF',
+          error: '#FF5252',
+          info: '#2196F3',
+          success: '#4CAF50',
+          warning: '#FFC107',
+        },
+      },
+    },
+  },
+})
